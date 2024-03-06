bdabb5a796a9ff489c20fcdfdae6402ddb131760
k4yt3x <i@k4yt3x.com>
2024-03-06 23:19:41 +0000

    feat(checkin): updated the checkin service to work with the latest binary
    Signed-off-by: k4yt3x <i@k4yt3x.com>

--- checkin/checkin/checkin.py
+++ checkin/checkin/checkin.py
@@ -9,36 +9,40 @@ app = Flask(__name__)
 
 @app.route("/validate", methods=["POST"])
 def validate():
-    # Ensure the request is JSON
-    if not request.is_json:
-        return jsonify({"error": "Request must be JSON"}), 400
+    try:
+        # ensure the request is JSON
+        if not request.is_json:
+            return jsonify({"error": "Request must be JSON"}), 400
 
-    data = request.get_json()
+        data = request.get_json()
 
-    # Validate the presence of 'name' and 'token' in the JSON payload
-    if "name" not in data or "token" not in data:
-        return jsonify({"error": "JSON must contain 'name' and 'token' fields"}), 400
+        # validate the presence of 'name' and 'token' in the JSON payload
+        if "token" not in data:
+            return (
+                jsonify({"error": "JSON must contain 'name' and 'token' fields"}),
+                400,
+            )
 
-    name = data["name"].encode("utf-8")  # Convert to bytes for C compatibility
-    token = data["token"]
+        token = data["token"]
 
-    # Ensure token is an integer
-    if not isinstance(token, int):
-        return jsonify({"error": "Token must be an integer"}), 400
+        # call the validate_token function
+        is_valid = (
+            subprocess.run(
+                ["../bin/validate", str(token)],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+                check=False,
+            ).returncode
+            == 0
+        )
 
-    # Call the validate_token function
-    is_valid = (
-        subprocess.run(
-            ["./validate", name, str(token)],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-            check=False,
-        ).returncode
-        == 0
-    )
-
-    # Return the result
-    return jsonify({"isValid": bool(is_valid)})
+        # return the result
+        return jsonify({"is_valid": bool(is_valid)})
+    except Exception:
+        return (
+            jsonify({"error": "An error occurred. Please notify the administrators."}),
+            500,
+        )
 
 
 def main():
